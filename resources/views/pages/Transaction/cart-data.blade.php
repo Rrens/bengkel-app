9e94b3bdd0b961e5c8805c18152b143ecf1553ea
Rrens <yusufrendi313@gmail.com>
2024-04-06 02:01:46 +0700

    Format number on sales

--- resources/views/pages/Transaction/cart-data.blade.php
+++ resources/views/pages/Transaction/cart-data.blade.php
@@ -4,26 +4,20 @@
             <td>{{ $loop->iteration }}</td>
             <td class="barcode">{{ $item->item[0]->barcode }}</td>
             <td>{{ $item->item[0]->name }}</td>
-            <td>{{ $item->price }}</td>
+            <td>{{ format_rupiah_tanpa_rp($item->price) }}</td>
             <td>{{ $item->jumlah_jual }}</td>
             <td>{{ $item->quantity }}</td>
             <td>{{ $item->discount_item }}</td>
-            <td id="total">{{ $item->total }}</td>
+            <td id="total">{{ format_rupiah_tanpa_rp($item->total) }}</td>
             <td class="text-center" width="160px">
                 <button id="update_cart" data-toggle="modal" data-target="#modal-item-edit"
-                    data-cartid="{{ $item->item_id }}"
-                    data-barcode="{{ $item->item[0]->barcode }}"
-                    data-product="{{ $item->item[0]->name }}"
-                    data-stock="{{ $item->item[0]->stock }}"
-                    data-price="{{ $item->price }}"
-                    data-jual="{{ $item->jumlah_jual }}"
-                    data-qty="{{ $item->quantity }}"
-                    data-discount="{{ $item->discount_item }}"
-                    data-total="{{ $item->total }}"
-                    class="btn btn-xs btn-primary">
+                    data-cartid="{{ $item->item_id }}" data-barcode="{{ $item->item[0]->barcode }}"
+                    data-product="{{ $item->item[0]->name }}" data-stock="{{ $item->item[0]->stock }}"
+                    data-price="{{ format_rupiah_tanpa_rp($item->price) }}" data-jual="{{ $item->jumlah_jual }}"
+                    data-qty="{{ $item->quantity }}" data-discount="{{ $item->discount_item }}"
+                    data-total="{{ format_rupiah_tanpa_rp($item->total) }}" class="btn btn-xs btn-primary">
                     <i class="fa fa-pencil"></i> Update
                 </button>
-                
                 <button id="del_cart" data-cartid="{{ $item->id }}" class="btn btn-xs btn-danger">
                     <i class="fa fa-trash"></i>Delete
                 </button>
